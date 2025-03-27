a8a7bad09a95cf34fa4de94cfc5ab6d4d99fcd73
Sandeep Gantasala <sandeepgantasala@Sandeeps-MacBook-Air.local>
2025-03-27 01:28:31 -0700

    Updated with Mixins

--- CourseAPP/views.py
+++ CourseAPP/views.py
@@ -6,8 +6,37 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
+from rest_framework import generics
+from rest_framework import mixins
 # Create your views here.
 
+class CourseList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+
+    queryset=Course.objects.all()
+    serializer_class=CourseSerializer
+
+    def get(self,request):
+        return self.list(request)
+    
+    def post(self,request):
+        return self.create(request)
+
+class CourseDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+
+    queryset=Course.objects.all()
+    serializer_class=CourseSerializer
+
+    def get(self,request,pk):
+        return self.retrieve(request,pk)
+    
+    def put(self,request,pk):
+        return self.update(request,pk)
+    
+    def delete(self,request,pk):
+        return self.destroy(request,pk)
+
+
+"""
 class CourseList(APIView):
 
     def get(self,request):
@@ -47,7 +76,7 @@ class CourseDetail(APIView):
             course=self.get_object(id)
             course.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
-
+"""
             
 
     
